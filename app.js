d4db16b9e4201ee1e62378c071a41c0994aeb1f8
Lorena C <criado.manzaneque@gmail.com>
2024-05-06 21:37:25 +0200

    Create calculator methods and update readme file

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
 class Calculator {
     constructor(operand1Element, operand2Element) {
-        this.operand1Element = operand1Element;
-        this.operand2Element = operand2Element;
-        this.clear();
+        this.operand1Element = operand1Element
+        this.operand2Element = operand2Element
+        this.clear()
     }
 
     clear() {
-        this.operand1 = 0;
-        this.operand2 = 0;
-        this.operator = '';
-        this.updateUI();
+        this.operand1 = 0
+        this.operand2 = 0
+        this.operator = ''
+        this.updateUI()
     }
 
     updateUI() {
-        this.operand1Element.innerHTML = this.operand1 + this.operator;
-        this.operand2Element.innerHTML = this.operand2;
+        this.operand1Element.innerHTML = this.operand1 + this.operator
+        this.operand2Element.innerHTML = this.operand2
     }
 
     appendNumber(number) {
         if (number === "." && this.operand2.includes(".")) return
         this.operand2 = this.operand2 === 0
             ? number
-            : this.operand2.toString() + number;
-        this.updateUI();
+            : this.operand2.toString() + number
+        this.updateUI()
     }
 
     delete() {
         if (this.operand2 === 0) return
         this.operand2 = +this.operand2.toString().slice(0, -1)
-        this.updateUI();
+        this.updateUI()
+    }
+
+    operation(operator) {
+        if (this.operator) {
+            this.calc()
+        }
+        this.operator = operator
+        this.operand1 = +this.operand2 === 0 ? this.operand1 : this.operand2
+        this.operand2 = 0
+        this.updateUI()
+    }
+
+    calc() {
+        switch (this.operator) {
+            case "+":
+                this.operand1 = +this.operand1 + +this.operand2
+                break
+            case "-":
+                this.operand1 = +this.operand1 - +this.operand2
+                break
+            case "×":
+                this.operand1 = +this.operand1 * +this.operand2
+                break
+            case "÷":
+                this.operand1 = +this.operand1 / +this.operand2
+                break
+        }
+        this.operator = ""
+        this.operand2 = 0
+        this.updateUI()
     }
 }
 
@@ -38,23 +68,32 @@ const operand2Element = document.querySelector("[data-operand-2]")
 const clearBtn = document.querySelector("[data-clear]")
 const numberBtns = document.querySelectorAll("[data-number]")
 const deleteBtn = document.querySelector("[data-delete]")
+const operationBtns = document.querySelectorAll("[data-operation]")
+const equalsBtn = document.querySelector("[data-equals]")
 
 // Create Calculator instance
 const calculator = new Calculator(operand1Element, operand2Element)
 
-// Clear button is activated
 clearBtn.addEventListener("click", () => {
     calculator.clear()
 })
 
-// Some number buttons is clicked
 numberBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         calculator.appendNumber(btn.innerHTML)
     })
 })
 
-// Delete button is activated
 deleteBtn.addEventListener("click", () => {
-    calculator.delete();
+    calculator.delete()
+})
+
+operationBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        calculator.operation(btn.innerHTML)
+    })
+})
+
+equalsBtn.addEventListener("click", () => {
+    calculator.calc()
 })
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # JS Calculator
 This project is a JavaScript calculator class designed to facilitate basic arithmetic operations within web applications. The class provides essential methods to perform calculations and update the user interface dynamically.
 
+# UML Calculator Diagram
+Represents the structure and functionality of the `Calculator` class.
+![UML_Calculator](images/calculator_uml.png)
+
 # Methods
 
 ## Constructor(operand1Element, operand2Element)
@@ -15,5 +19,13 @@ Updates the content of the DOM elements `operand1Element` and `operand2Element` 
 ## AppendNumber(number)
 Receives an argument `number`, which is the number to be added to the `operand2`. It checks if `operand2` already contains a decimal point, and if so, it avoids adding another decimal point. If the current value of `operand2` is `0`, it replaces it with the new `number`. Then, it updates `operand2` by concatenating the number to the end of its current value as a string. After it calls the `updateUI()` method to reflect the changes.
 
-## Delete():
+## Delete()
 Converts the `operand2` to a string, removes the last character, and then converts the resulting string back to a number. After deletion, it calls the `updateUI()` method.
+
+## Operations
+
+## Operation(operator)
+Takes an `operator`, representing the mathematical operator selected by the user. If there's already an `operator` in use, it calls the calc() method to perform the previous operation. Then, it assigns the new `operator` and prepares the operands for the next operation. Finally, it updates the user interface to reflect the changes.
+
+## Calc()
+Uses the current `operator` and operands to carry out the corresponding mathematical operation. After calculating the result, it updates `operand1` with the result and resets the `operand2` and the `operator`. Finally, it updates the user interface.
